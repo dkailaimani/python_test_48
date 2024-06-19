feff8a3ee854e6d5adb6ba52f7d8f438ac474e7a
dkailaimani <102199241+dkailaimani@users.noreply.github.com>
2024-06-19 08:26:31 -0500

    Create contact_component.jsx

---
+++ contact_component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <section className="contact">
+        <h2>Contact Me</h2>
+        <p>Email: deetechie22@gmail.com</p> 
+        <p>LinkedIn: linkedin.com/in/dkailap</p> 
+      </section>
+    );
+  }
+}
+
+export default Contact;
