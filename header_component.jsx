540876c0d09450c264abba0135d0550702eb560e
dkailaimani <102199241+dkailaimani@users.noreply.github.com>
2024-06-19 08:23:55 -0500

    Create header_component.jsx

---
+++ header_component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-overlay">
+        <h1>DKaila Price</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
